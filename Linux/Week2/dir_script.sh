cae696c14202bde89ad0af4b76ac4e538b5d4f03
JoYoonHye <155337921+ynhea@users.noreply.github.com>
2026-09-03 19:02:56 +0900

    fix: dir_script.sh
    - 변수 추가
    - 리포트 저장
    - 따옴표를 이용한 안정성 강화
    - 함수 리펙토링

--- Linux/Week2/dir_script.sh
+++ Linux/Week2/dir_script.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
+txt_link=~/Workspace/report_backup_$(date "+%Y%m%d").txt
+func() {
+	echo "=== $1 ===" | tee -a "$txt_link"
+}
+
+func "0. umask 설정"
 cd ~/Workspace
+umask 077 
+umask | tee -a "$txt_link"
 
-# 1. 백업 대상 파일 목록 확인
-backup_dir=~/Workspace/practice
-link=backup_$(date "+%Y%m%d").tar.gz
-find $backup_dir
+func "1. 백업 대상 파일 목록 확인"
+find "$1" | tee -a "$txt_link"
+link="backup_$(date "+%Y%m%d").tar.gz"
 
-# 2. 압축
-mkdir -p BackUp
-tar -czvf $link -C ~/Workspace practice 
-cp $link  BackUp/$link
+func "2. 압축"
+mkdir -p "$2"
+tar -czvf "$link" "$1"
+cp "$link" "$2/$link"
+ls -l "$2" | tee -a "$txt_link"
 
-# 3. 권한 제한
-chmod 600 BackUp/$link
+func "3. 권한 설정"
+chmod 600 "$2/$link"
+sudo chown "$(whoami)" "$2/$link"
+sudo chgrp "$(whoami)" "$2/$link"
+ls -l "$2/$link" | tee -a "$txt_link"
 
-# 4. 백업 성공확인
-ls -l BackUp
-tar -tzf BackUp/$link
+func "4. 백업 성공확인"
+ls -l "$2" | tee -a "$txt_link"
 
-# 5. 임시 압축파일 삭제
-rm $link
+func "5. 백업 내용확인" 
+tar -tzf "$2/$link" | tee -a "$txt_link"
+
+func "6. 임시 압축파일 삭제"
+rm -f "$link"
