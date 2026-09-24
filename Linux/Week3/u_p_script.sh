30e98c86e099948051b2476b642a670d1a1efc4d
JoYoonHye <155337921+ynhea@users.noreply.github.com>
2026-09-24 23:11:19 +0900

    fix: u_p_script.sh
    생성할 사용자 이름을 첫 번째 인자로 받기 (./diagnose.sh testuser)
    검색할 프로세스 이름을 두 번째 인자로 받기 (./diagnose.sh testuser sleep)
    해당 프로세스가 발견되면 PID와 함께 "종료하시겠습니까?"에 해당하는 안내 메시지만 출력 (실제 kill은 자동 실행 X)
    실행 결과를 diagnose_log_날짜.txt로 저장

--- Linux/Week3/u_p_script.sh
+++ Linux/Week3/u_p_script.sh
@@ -1,15 +1,38 @@
 #!/bin/bash
 
-sudo useradd -m -s /bin/bash testuser
+USER_NAME=$1
+PROCESS_NAME=$2
+
+LOG_FILE=~/Workspace/report_u_p_$(date "+%Y%m%d").txt
+func() {
+	echo "=== $1 ===" | tee -a $LOG_FILE
+}
+
+
+func "0. 새 사용자와 그룹 생성"
+sudo useradd -m -s /bin/bash "$USER_NAME"
 sudo groupadd testgroup
 
-grep "testuser" /etc/passwd
-grep "testgroup" /etc/group
+grep "$USER_NAME" /etc/passwd | tee -a "$LOG_FILE"
+grep "testgroup" /etc/group | tee -a "$LOG_FILE"
 
-sudo usermod -aG testgroup testuser
-groups testuser
 
-ps aux --sort=-%cpu | head -6
+func "1. 그룹 추가"
+sudo usermod -aG testgroup "$USER_NAME"
+groups "$USER_NAME" | tee -a "$LOG_FILE"
 
-sleep 60 &
-ps aux | grep sleep | grep -v grep
+
+func "2. CPU 사용량 상위 5개 프로세스"
+ps aux --sort=-%cpu | head -6 | tee -a "$LOG_FILE"
+
+func "3. 검색한 이름을 가진 프로세스의 PID"
+pid=$(ps aux | grep "$PROCESS_NAME" | grep -v grep | awk '{print $2}')
+if [ -n "$pid" ]; then
+    echo "프로세스 '$PROCESS_NAME'가 발견되었습니다." | tee -a "$LOG_FILE"
+    echo "PID : $pid" | tee -a "$LOG_FILE"
+    echo "종료하시겠습니까?"
+else
+    echo "프로세스 '$PROCESS_NAME'가 발견되지 않았습니다." | tee -a "$LOG_FILE"
+fi
+
+echo "실행 결과가 $LOG_FILE 에 저장되었습니다."
